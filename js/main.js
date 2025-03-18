edc6ae17ef482c1e140f486e8623bd9b58cbd9bb
casspb <cassidypelacek@gmail.com>
2025-03-18 15:38:12 -0400

    still figuring out OOPs
    Working to try to make it so that I dont have to put the SVG's in the js and instead just have them in the code so it doesnt change any of the styling - adding ids to to individual svgs that represent the dinosaurs - hoping to adjust it more so that those ids select the content with vue later

--- js/main.js
+++ js/main.js
@@ -1,76 +1,81 @@
-import { Dinosaur } from "./dinosaur";
+// main.js or your Vue entry file
+import { createApp } from 'vue';
+import { Dinosaur } from './dinosaur.js';
 
-const ankylosaurus = new Dinosaur(
+const Ankylosaurus = new Dinosaur(
     "Ankylosaurus",
     "1.8 meters",
     "7.6 meters",
     "Herbivorous",
     "Defensive, Stoic, Loyal",
     "An-kee-loh-sore-us"
-    //appearance here
-);
+  );
+  const Apatosaurus = new Dinosaur(
+    "Apatosaurus",
+    "4.6 meters",
+    "21 meters",
+    "Herbivorous",
+    "Gentle, Slow-moving",
+    "A-pa-toh-sore-us"
+  );
+  const Baryonyx = new Dinosaur(
+    "Baryonyx",
+    "3 meters",
+    "9 meters",
+    "Carnivorous",
+    "Aggressive, Territorial",
+    "Bar-ee-oh-nix"
+  );
+  const Edomtosaurus = new Dinosaur(
+      "Edomtosaurus"
+  );
+  const Gallimimus = new Dinosaur(
+      "Gallimimus"
+  );
+  const InRex = new Dinosaur(
+      "Indominus Rex"
+  );
+  const Metricanthosaurus = new Dinosaur(
+      "Metricanthosaurus"
+  );
+  const Microceratus = new Dinosaur(
+      "Microceratus"
+  );
+  const Mosasaurus = new Dinosaur(
+      "Mosasaurus"
+  );
+  const Pachychephalosaurus = new Dinosaur(
+      "Pachychephalosaurus"
+  );
+  const Parasaurolophus = new Dinosaur(
+      "Parasaurolophus"
+  );
+  const Pteranodon = new Dinosaur(
+      "Pteranodon"
+  );
+  const Stegosaurus = new Dinosaur(
+      "Stegosaurus"
+  );
+  const Suchomimus = new Dinosaur(
+      "Suchomimus"
+  );
+  const Triceratops = new Dinosaur(
+      "Triceratops"
+  );
+//   const Trex = new Dinosaur(
+//       "Tyrannosaurus Rex"
+//   );
+//   const Velociraptor = new Dinosaur(
+//       "Velociraptor"
+//   );
 
-const apatosaurus = new Dinosaur(
- 
-);
 
-const baryonyx = new Dinosaur(
- 
-);
+const app = createApp({
+  data() {
+    return {
+       
 
-const dimorphodon = new Dinosaur(
- 
-);
-
-const edomtosaurus = new Dinosaur(
- 
-);
-
-const gallimimus = new Dinosaur(
- 
-);
-
-const indominusRex = new Dinosaur(
- 
-);
-
-const metricanthosaurus = new Dinosaur(
- 
-);
-
-const microceratus = new Dinosaur(
- 
-);
-
-const mosasaurus = new Dinosaur(
- 
-);
-
-const pachychephalosaurus = new Dinosaur(
- 
-);
-
-const stegosaurus = new Dinosaur(
- 
-);
-
-const pteranodon = new Dinosaur(
- 
-);
-
-const suchomimus = new Dinosaur(
- 
-);
-
-const triceratops = new Dinosaur(
- 
-);
-
-const Trex = new Dinosaur(
- 
-);
-
-const velociraptor = new Dinosaur(
- 
-);
+    };
+  }
+}).mount('#app');
 
