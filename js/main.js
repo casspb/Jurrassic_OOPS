2d7633a1df3b89dbfc05ccabca1023071d67d973
casspb <cassidypelacek@gmail.com>
2025-03-18 14:47:21 -0400

    starting of OOP
    start file of oop

---
+++ js/main.js
@@ -0,0 +1,76 @@
+import { Dinosaur } from "./dinosaur";
+
+const ankylosaurus = new Dinosaur(
+    "Ankylosaurus",
+    "1.8 meters",
+    "7.6 meters",
+    "Herbivorous",
+    "Defensive, Stoic, Loyal",
+    "An-kee-loh-sore-us"
+    //appearance here
+);
+
+const apatosaurus = new Dinosaur(
+ 
+);
+
+const baryonyx = new Dinosaur(
+ 
+);
+
+const dimorphodon = new Dinosaur(
+ 
+);
+
+const edomtosaurus = new Dinosaur(
+ 
+);
+
+const gallimimus = new Dinosaur(
+ 
+);
+
+const indominusRex = new Dinosaur(
+ 
+);
+
+const metricanthosaurus = new Dinosaur(
+ 
+);
+
+const microceratus = new Dinosaur(
+ 
+);
+
+const mosasaurus = new Dinosaur(
+ 
+);
+
+const pachychephalosaurus = new Dinosaur(
+ 
+);
+
+const stegosaurus = new Dinosaur(
+ 
+);
+
+const pteranodon = new Dinosaur(
+ 
+);
+
+const suchomimus = new Dinosaur(
+ 
+);
+
+const triceratops = new Dinosaur(
+ 
+);
+
+const Trex = new Dinosaur(
+ 
+);
+
+const velociraptor = new Dinosaur(
+ 
+);
+
